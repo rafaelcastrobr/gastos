2fedc725487756c82f8e1e9c02a2d0f99cddb7c1
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-06-18 01:42:47 -0300

    alteracao de alguns detalhes {img, css-input-mes}

--- src/Components/InserirDados.jsx
+++ src/Components/InserirDados.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { ContentContext } from "../Contexts/ContentProvider"
 import { geraId } from "../Func/geraId"
 import { salvaDados } from "../Func/salvaDados"
+import money from '../img/money.png'
 
 
 export default function InserirDados() {
@@ -22,9 +23,9 @@ export default function InserirDados() {
   function handleClickAdd(e) {
 
     if (mes === '') return dispatch({ type: 'ERRO_MES' })
-    if (descricao === '' || valor === '') return dispatch({ type: 'ERRO_VALORES'})
+    if (descricao === '' || valor === '') return dispatch({ type: 'ERRO_VALORES' })
 
-    
+
 
     dispatch({ type: 'GERAID', payload: geraId() })
 
@@ -36,21 +37,21 @@ export default function InserirDados() {
 
     salvaDados(dados, mes)
 
-    dispatch({ type: 'ADICIONA_DADOS', mes: mes})
+    dispatch({ type: 'ADICIONA_DADOS', mes: mes })
   }
 
 
   return (
     <div className="container">
       <h1>ADICIONE OS GASTOS</h1>
+      <img src={money} alt="money" className="img-money" />
       <div className="input-mes">
         <div>
           <span style={{ color: 'red' }}>{erros.mes.toggle && erros.mes.menssagem}</span>
         </div>
-        <div style={{display: 'flex', flexDirections: 'row'}}>
-
-        <span style={{display: 'flex', alignItems: 'center'}}><input type="radio" style={{width: '20px', height: '25px'}} value="dados" onChange={escolheMes} checked={mes === 'dados'} />Mês Atual</span>
-        <span style={{display: 'flex', alignItems: 'center'}}><input type="radio" style={{width: '20px', height: '25px'}} value="dadosProxMes" onChange={escolheMes} checked={mes === 'dadosProxMes'} />Próximo Mês</span>
+        <div className="input-box-mes">
+          <div className="div-input-mes"><input type="radio" style={{ width: '20px', height: '25px' }} value="dados" onChange={escolheMes} checked={mes === 'dados'} />Mês Atual</div>
+          <div className="div-input-mes"><input type="radio" style={{ width: '20px', height: '25px' }} value="dadosProxMes" onChange={escolheMes} checked={mes === 'dadosProxMes'} />Próximo Mês</div>
         </div>
       </div>
       <span style={{ color: 'red' }}>{erros.valores.toggle && erros.valores.menssagem}</span>
