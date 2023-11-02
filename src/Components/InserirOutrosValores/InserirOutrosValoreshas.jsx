7a2003d5664c5193ef1956d7a0f013c2b3ccd24d
rafaelcastrobr <rafaelcastrobr@gmail.com>
2023-11-02 02:01:25 -0300

    feat: tirar exibiçao de quando lista nao tiver

--- src/Components/InserirOutrosValores/InserirOutrosValoreshas.jsx
+++ src/Components/InserirOutrosValores/InserirOutrosValoreshas.jsx
@@ -12,31 +12,35 @@ export default function InserirOutrosValores() {
     dispatch({ type: 'EXCLUI_LINHA', linha: linha })
   }
 
-  function handleClickMesAtual (e) {
+  function handleClickMesAtual(e) {
     const linha = e.target.parentNode
 
-    dispatch({ type: 'PASSA_PARA_MES_ATUAL', linha: linha})
+    dispatch({ type: 'PASSA_PARA_MES_ATUAL', linha: linha })
   }
 
   return (
     <>
-      <div>
-        <h3 style={{color: 'green'}}>Outros valores</h3>
-        <hr />
-        <ul>
-          {outrosDados && outrosDados.map(el => {
-            return (
-              <>
-                <li name="outrosDados" className="list-organizada" key={el.id} id={el.id}>
-                  <button className="btn-excluir" onClick={handleClickRemove}>X</button>
-                  <button className="btn-passar-prox-mes" onClick={handleClickMesAtual} >^</button>
-                  <p>{el.descricao} * <span style={{ color: el.valor.includes('-') ? 'red' : 'blue' }}>{(+el.valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span></p>
-                </li>
-              </>
-            )
-          })}
-        </ul>
-      </div>
+      {outrosDados === null || outrosDados.length === 0 ? '' :
+        <>
+          <div>
+            <h3 style={{ color: 'green' }}>Outros valores</h3>
+            <hr />
+            <ul>
+              {outrosDados && outrosDados.map(el => {
+                return (
+                  <>
+                    <li name="outrosDados" className="list-organizada" key={el.id} id={el.id}>
+                      <button className="btn-excluir" onClick={handleClickRemove}>X</button>
+                      <button className="btn-passar-prox-mes" onClick={handleClickMesAtual} >^</button>
+                      <p>{el.descricao} * <span style={{ color: el.valor.includes('-') ? 'red' : 'blue' }}>{(+el.valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span></p>
+                    </li>
+                  </>
+                )
+              })}
+            </ul>
+          </div>
+        </>
+      }
     </>
   )
 }
